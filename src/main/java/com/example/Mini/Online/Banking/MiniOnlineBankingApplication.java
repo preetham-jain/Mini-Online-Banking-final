5e30bdc0468028a5ac00b812d4c5f9971140eb8a
Preetham Jain <preethamjain@Preethams-MacBook-Pro.local>
2021-03-02 15:38:44 +0530

    first commit

---
+++ src/main/java/com/example/Mini/Online/Banking/MiniOnlineBankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mini.Online.Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniOnlineBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mini/Online/Banking/MiniOnlineBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mini.Online.Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOnlineBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
